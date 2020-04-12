9b7dbea37d0064e39723940c4fc10a3def1796f3
Victor Ibañez <victor.ibanez@flowable.com>
2020-04-12 22:26:23 +0200

    Berte's task

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -7,3 +7,12 @@ function answer() {
         inputText.value = '';
     }
 }
+
+// TODO: Define new function to handler clear button
+function clearInput () {
+    // TODO: When the event is received, you should remove all content of input
+    // TODO: Define new let variable to get input element
+    // TODO: Conditional if value is not empty --> remove all
+
+    // Tip: document.getElementById("inputtext").inputText.value = '';
+}
