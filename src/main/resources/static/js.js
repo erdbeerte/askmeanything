37a3561759f23f68b392dbe077aebd1e8b2d390a
berte.fleissig@gmail.com <Berte.Fleissig@gmail.com>
2020-04-13 17:25:22 +0200

    1-clear button

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -4,15 +4,13 @@ function answer() {
 
     if (inputText.value != '') {
         answerdiv.hidden = false;
-        inputText.value = '';
     }
 }
 
-// TODO: Define new function to handler clear button
-function clearInput () {
-    // TODO: When the event is received, you should remove all content of input
-    // TODO: Define new let variable to get input element
-    // TODO: Conditional if value is not empty --> remove all
+function clearInput() {
+    let inputText = document.getElementById("inputtext");
+    let answerdiv = document.getElementById("answerid");
 
-    // Tip: document.getElementById("inputtext").inputText.value = '';
+    inputText.value = '';
+    answerdiv.hidden = true;
 }
