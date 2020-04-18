6ec73f673246cdd6b8c1b0f2b631008bfb78ffcf
Victor Ibañez <victor.ibanez@flowable.com>
2020-04-18 17:52:41 +0200

    docs folder

---
+++ docs/js.js
@@ -0,0 +1,16 @@
+function answer () {
+    let inputText = document.getElementById('inputtext');
+    let answerdiv = document.getElementById('answerid');
+
+    if (inputText.value != '') {
+        answerdiv.hidden = false;
+    }
+}
+
+function clearInput () {
+    let inputText = document.getElementById('inputtext');
+    let answerdiv = document.getElementById('answerid');
+
+    inputText.value = '';
+    answerdiv.hidden = true;
+}
